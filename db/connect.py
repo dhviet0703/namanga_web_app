1f740bedb692d440e409ca34b39021d235a0f9dc
vietgym <vietgym007@gmail.com>
2024-05-12 12:25:32 +0700

    feat: auth

---
+++ db/connect.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+conn = st.connection("postgresql", type="sql")
--- hello.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-st.title("Hello World")
-st.write("This is a sample app")
-x = st.text_input("Enter a text")
-if x:
-    st.write(x)
